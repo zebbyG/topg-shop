6c1765ab7ae633c0992bd4e59782bea48418cf34
zebbyG <zebbynjau@gmail.com>
2023-05-11 22:35:58 +0300

    default files for the main project

---
+++ TOP Gz SHOE SHOP/the_shop/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for the_shop project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.top_shop, name='top_shop')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='top_shop')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import debug_toolbar
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('', include('top_shop.urls')),
+    path('shop_services/', include('shop_services.urls')),
+    path('brands/', include('brands.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
