35f660ba225c59819d2f7d464e72a1d1a57c628b
zebbyG <zebbynjau@gmail.com>
2023-05-11 22:19:48 +0300

    urls for the topshop app

---
+++ TOP Gz SHOE SHOP/top_shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'introPage'
+
+urlpatterns = [
+    path('', views.home, name="home-page"),
+    path('about/', views.about, name="about-page"),
+    path('contacts/', views.contacts, name="contacts-page"),
+    path('team/', views.team, name="team-page"),
+    path('the_store/', views.catalogue, name="catalogue-page")
+]
