e8538889db15e95cde900678e22d5ad443b0f45e
zebbyG <zebbynjau@gmail.com>
2023-06-06 13:09:39 +0300

    rm unneccessary fields in forms

--- TOP Gz SHOE SHOP/accounts/forms.py
+++ TOP Gz SHOE SHOP/accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -10,7 +10,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email')
 
 
 class EditProfileForm(UserChangeForm):
@@ -22,10 +22,3 @@ class EditProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
-
-
-class EditPasswordForm(PasswordChangeForm):
-
-    class Meta:
-        model = User
-        fields = ('old_password', 'new_password', 'confirm_password')
