1d33909e5b711b2e5d488482279167dc38b53a82
zebbyG <zebbynjau@gmail.com>
2023-06-06 11:18:35 +0300

    profile details url

--- TOP Gz SHOE SHOP/accounts/urls.py
+++ TOP Gz SHOE SHOP/accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
@@ -9,6 +8,7 @@ urlpatterns = [
     path('sign_up/', views.sign_up, name="sign_up"),
     path('log_in/', views.log_in, name="log_in"),
     path('edit_profile/', views.edit_profile, name="edit_profile"),
-    path('password/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('profile_details/', views.profile_details, name="profile_details"),
+    path('password_change/', views.password_change, name='password_change'),
     path('log_out/', views.log_out, name="log_out")
 ]
