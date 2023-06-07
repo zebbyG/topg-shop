b0e1560b9064b88f31464afc5d802ad833d95675
zebbyG <zebbynjau@gmail.com>
2023-06-07 15:50:52 +0300

    changed profile-picture

--- TOP Gz SHOE SHOP/accounts/forms.py
+++ TOP Gz SHOE SHOP/accounts/forms.py
@@ -7,10 +7,11 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    profile_pic = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email', 'profile_pic')
 
 
 class EditProfileForm(UserChangeForm):
@@ -18,10 +19,11 @@ class EditProfileForm(UserChangeForm):
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    profile_picture = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email', 'profile_picture')
 
 
 class ChangePasswordForm(PasswordChangeForm):
