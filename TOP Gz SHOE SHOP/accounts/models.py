01131986c6680c2de5fff62f9ebbc36c249fae87
zebbyG <zebbynjau@gmail.com>
2023-06-07 15:27:49 +0300

    add rules to password change

--- TOP Gz SHOE SHOP/accounts/models.py
+++ TOP Gz SHOE SHOP/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    profile_picture = models.ImageField(upload_to='user_profiles/', height_field='image_height', width_field='image_width')
+    profile_picture = models.ImageField(upload_to='user_profiles/', height_field='image_height', width_field='image_width', null=True, blank=True)
     image_height = models.PositiveIntegerField(blank=True, null=True)
     image_width = models.PositiveIntegerField(blank=True, null=True)
 
