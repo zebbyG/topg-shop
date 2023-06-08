53a4bad34c0f0c11c010f1d32b526cd606170a7c
zebbyG <zebbynjau@gmail.com>
2023-06-08 15:20:41 +0300

    path to profile deletion

--- TOP Gz SHOE SHOP/accounts/urls.py
+++ TOP Gz SHOE SHOP/accounts/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('edit_profile/', views.edit_profile, name="edit_profile"),
     path('profile_details/', views.profile_details, name="profile_details"),
     path('password_change/', views.password_change, name='password_change'),
+    path('delete_profile_picture/', views.delete_profile_picture, name="delete_profile_picture"),
     path('log_out/', views.log_out, name="log_out")
 ]
