784c52f2ca3d86f7721a8daa9ac6ed4856f0b631
zebbyG <zebbynjau@gmail.com>
2023-06-14 13:14:45 +0300

    checkout views

--- TOP Gz SHOE SHOP/brands/views.py
+++ TOP Gz SHOE SHOP/brands/views.py
@@ -48,3 +48,7 @@ def shoes_accessories(request):
 @login_required(login_url='accounts:log_in')
 def add_to_cart(request):
     return render(request, 'add_to_cart.html')
+
+
+def check_out(request):
+    return render(request, 'check_out.html')
