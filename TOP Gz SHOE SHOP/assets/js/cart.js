9d82abf6eb2b55de0f3ec2ab91d35733ca4973c5
zebbyG <zebbynjau@gmail.com>
2023-06-22 12:11:00 +0300

    add to cart functionality

--- TOP Gz SHOE SHOP/assets/js/cart.js
+++ TOP Gz SHOE SHOP/assets/js/cart.js
@@ -1,10 +1,26 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart');
 
+function handleAddToCart(event) {
+  event.preventDefault();
 
-for(var i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
-        var productId = this.dataset.product;
-        var action = this.dataset.action;
-        console.log('productId:', productId, 'action:', action)
-    })
+  // Check if the user is authenticated
+  if (!user) {
+    // Redirect the user to the login page
+    var nextUrl = encodeURIComponent(window.location.href);
+    window.location.href = "{% url 'accounts:log_in' %}?next=" + nextUrl;
+    return;
+  }
+
+  // Proceed with adding the item to the cart
+  var productId = this.dataset.product;
+  var action = this.dataset.action;
+  console.log('productId:', productId, 'action:', action);
+  console.log('USER:', user);
+
+  // Add your logic to update the cart here
+  // ...
+}
+
+for (var i = 0; i < updateBtns.length; i++) {
+  updateBtns[i].addEventListener('click', handleAddToCart);
 }
