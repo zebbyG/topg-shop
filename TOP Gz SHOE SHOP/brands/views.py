15655d729cb867a579ef07007bd514eedbe09edd
zebbyG <zebbynjau@gmail.com>
2023-06-22 14:09:45 +0300

    change the order of displaying the products to "-shoe_in_stock, -shoe_time_added"

--- TOP Gz SHOE SHOP/brands/views.py
+++ TOP Gz SHOE SHOP/brands/views.py
@@ -4,41 +4,41 @@ from django.shortcuts import get_object_or_404
 
 
 def catalogue(request):
-    shop_brands = Brand.objects.all().order_by('-brand_in_stock')
+    shop_brands = Brand.objects.all().order_by('-brand_in_stock', '-time')
     return render(request, 'catalogue.html', {"shop_brands": shop_brands})
 
 
 def jordans(request):
     jordan_brand = get_object_or_404(Brand, brand_name='Jordans')
-    jordan_shoes = Product.objects.filter(brand=jordan_brand).order_by('-shoe_in_stock')
+    jordan_shoes = Product.objects.filter(brand=jordan_brand).order_by('-shoe_in_stock', '-shoe_time_added')
     return render(request, 'jordans.html', {"jordan_shoes": jordan_shoes})
 
 
 def nikes(request):
     nike_brand = get_object_or_404(Brand, brand_name='Nikes')
-    nike_shoes = Product.objects.filter(brand=nike_brand).order_by('-shoe_in_stock')
+    nike_shoes = Product.objects.filter(brand=nike_brand).order_by('-shoe_in_stock', '-shoe_time_added')
     return render(request, 'nikes.html', {"nike_shoes": nike_shoes})
 
 
 def vans(request):
     vans_brand = get_object_or_404(Brand, brand_name='Vans')
-    vans_shoes = Product.objects.filter(brand=vans_brand).order_by('-shoe_in_stock')
+    vans_shoes = Product.objects.filter(brand=vans_brand).order_by('-shoe_in_stock', '-shoe_time_added')
     return render(request, 'vans.html', {"vans_shoes": vans_shoes})
 
 
 def yeezys(request):
     yeezy_brand = get_object_or_404(Brand, brand_name='Yeezys')
-    yeezy_shoes = Product.objects.filter(brand=yeezy_brand).order_by('-shoe_in_stock')
+    yeezy_shoes = Product.objects.filter(brand=yeezy_brand).order_by('-shoe_in_stock', '-shoe_time_added')
     return render(request, 'yeezys.html', {"yeezy_shoes": yeezy_shoes})
 
 
 def pradas(request):
     prada_brand = get_object_or_404(Brand, brand_name='Pradas')
-    prada_shoes = Product.objects.filter(brand=prada_brand).order_by('-shoe_in_stock')
+    prada_shoes = Product.objects.filter(brand=prada_brand).order_by('-shoe_in_stock', '-shoe_time_added')
     return render(request, 'pradas.html', {"prada_shoes": prada_shoes})
 
 
 def shoes_accessories(request):
     shoes_accessories_brand = get_object_or_404(Brand, brand_name='shoes_accessories')
-    shoes_accessories_products = Product.objects.filter(brand=shoes_accessories_brand).order_by('-shoe_in_stock')
+    shoes_accessories_products = Product.objects.filter(brand=shoes_accessories_brand).order_by('-shoe_in_stock', '-shoe_time_added')
     return render(request, 'shoes_accessories.html', {"shoes_accessories_products": shoes_accessories_products})
