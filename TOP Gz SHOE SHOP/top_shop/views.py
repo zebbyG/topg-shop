a49c2f052da20d66a1080b87137cd5e9063b1e31
zebbyG <zebbynjau@gmail.com>
2023-06-26 12:37:15 +0300

    display cart items in every view

--- TOP Gz SHOE SHOP/top_shop/views.py
+++ TOP Gz SHOE SHOP/top_shop/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render
+from theorders.models import Order
 
 
 def home(request):
-    return render(request, 'home-shop.html', {"name": "Zebbylion Njau"})
-
-
-def catalogue(request):
-    return render(request, 'accessories.html', {"name": "Zebbylion Njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'home-shop.html', {"order": order})
+    else:
+        return render(request, 'home-shop.html')
 
 
 def about(request):
-    return render(request, 'about-shop.html', {"name": "Zebbylion Njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'about-shop.html', {"order": order})
+    else:
+        return render(request, 'about-shop.html')
 
 
 def contacts(request):
-    return render(request, 'contacts.html', {"name": "Zebbylion Njau"})
-
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'contacts.html', {"order": order})
+    else:
+        return render(request, 'contacts.html')
 
 def team(request):
-    return render(request, 'team.html', {"name": "Zebbylion Njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'team.html', {"order": order})
+    else:
+        return render(request, 'team.html')
