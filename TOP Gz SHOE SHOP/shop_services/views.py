c5d9cdc702bcf07d6e10d97578b1e63713877ec4
zebbyG <zebbynjau@gmail.com>
2023-06-26 12:37:25 +0300

    display cart items in every view

--- TOP Gz SHOE SHOP/shop_services/views.py
+++ TOP Gz SHOE SHOP/shop_services/views.py
@@ -1,27 +1,51 @@
 from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
+from theorders.models import Order
 
 
 def services(request):
-    return render(request, 'shop-services.html', {"name": "Zebbylion Njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'shop-services.html', {"order": order})
+    return render(request, 'shop-services.html')
 
 
 def order_shoes(request):
-    return render(request, 'order-shoes.html', {"name": "Zebbylion Njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'order-shoes.html', {"order": order})
+    return render(request, 'order-shoes.html')
 
 
 def trade(request):
-    return render(request, 'trade.html', {"name": "Zebbylion Njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'trade.html', {"order": order})
+    return render(request, 'trade.html')
 
 
 def customize_shoes(request):
-    return render(request, 'customize_shoes.html', {"name": "Zebbylion Njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'customize_shoes.html', {"order": order})
+    return render(request, 'customize_shoes.html')
 
 
 def donate_shoes(request):
-    return render(request, 'donate_shoes.html', {"name": "Zebbylion Njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'donate_shoes.html', {"order": order})
+    return render(request, 'donate_shoes.html')
 
 
 def customer_service(request):
-    return render(request, 'customer_service.html', {"name": "Zebbylion njau"})
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(user=customer, complete=False)
+        return render(request, 'customer_service.html', {"order": order})
+    return render(request, 'customer_service.html')
 
