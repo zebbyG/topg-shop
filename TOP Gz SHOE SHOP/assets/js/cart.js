de165a837d401aa5477233227cae83c4130212e8
zebbyG <zebbynjau@gmail.com>
2023-06-26 10:54:29 +0300

    log user and productId to console

--- TOP Gz SHOE SHOP/assets/js/cart.js
+++ TOP Gz SHOE SHOP/assets/js/cart.js
@@ -1,26 +1,12 @@
-var updateBtns = document.getElementsByClassName('update-cart');
+var updateBtns = document.getElementsByClassName('update-cart')
 
-function handleAddToCart(event) {
-  event.preventDefault();
 
-  // Check if the user is authenticated
-  if (!user) {
-    // Redirect the user to the login page
-    var nextUrl = encodeURIComponent(window.location.href);
-    window.location.href = "{% url 'accounts:log_in' %}?next=" + nextUrl;
-    return;
-  }
+for(var i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log('productId:', productId, 'action:', action)
 
-  // Proceed with adding the item to the cart
-  var productId = this.dataset.product;
-  var action = this.dataset.action;
-  console.log('productId:', productId, 'action:', action);
-  console.log('USER:', user);
-
-  // Add your logic to update the cart here
-  // ...
-}
-
-for (var i = 0; i < updateBtns.length; i++) {
-  updateBtns[i].addEventListener('click', handleAddToCart);
+        console.log('User:', user)
+    })
 }
