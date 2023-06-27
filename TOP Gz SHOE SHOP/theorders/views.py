38f1b71522fa3002aad2979e5f5b215fe1a37284
zebbyG <zebbynjau@gmail.com>
2023-06-27 12:40:48 +0300

    add a view to handle cart updates

--- TOP Gz SHOE SHOP/theorders/views.py
+++ TOP Gz SHOE SHOP/theorders/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import json
+from django.http import JsonResponse
 from .models import *
+from brands.models import Product
 
 
 @login_required(login_url='accounts:log_in')
@@ -17,3 +20,29 @@ def check_out(request):
     order, created = Order.objects.get_or_create(user=customer, complete=False)
     items = order.orderitem_set.all()
     return render(request, 'check_out.html', {"items": items, "order": order})
+
+
+def update_item(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print("ProductId:", productId)
+    print("Action:", action)
+
+    customer = request.user
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(user=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = orderItem.quantity + 1
+    elif action == 'remove':
+        orderItem.quantity = orderItem.quantity -1
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('item added successfully', safe=False)
