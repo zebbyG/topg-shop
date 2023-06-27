d06fb3609133abb3009789b959a8e639d6905efa
zebbyG <zebbynjau@gmail.com>
2023-06-27 12:40:03 +0300

    add url to update items to cart

--- TOP Gz SHOE SHOP/theorders/urls.py
+++ TOP Gz SHOE SHOP/theorders/urls.py
@@ -7,4 +7,5 @@ app_name = 'orders'
 urlpatterns = [
     path('add_to_cart/', views.add_to_cart, name="add_to_cart"),
     path('checkout/', views.check_out, name="checkout"),
+    path('update_item/', views.update_item, name="update_item"),
 ]
