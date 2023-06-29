9fad9a9688dfc1e8f115af69ead67fe225fd539c
zebbyG <zebbynjau@gmail.com>
2023-06-29 10:31:39 +0300

    csrf token to allow posting to the db

---
+++ TOP Gz SHOE SHOP/assets/js/csrf_token.js
@@ -0,0 +1,18 @@
+var user = '{{ request.user }}';
+
+  function getToken(name) {
+    var tokenValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does the cookie begin with the name we want
+            if (cookie.substring(0, name.length + 1) === (name + '=')){
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+  }
+  var csrftoken = getToken('csrftoken')
