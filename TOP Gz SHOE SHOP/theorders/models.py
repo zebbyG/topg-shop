480a710f00078cf08c28b04f6c52b7fc2b761241
zebbyG <zebbynjau@gmail.com>
2023-07-05 17:56:41 +0300

    alter auto_now_add to auto_now, add delivery status and time delivered

--- TOP Gz SHOE SHOP/theorders/models.py
+++ TOP Gz SHOE SHOP/theorders/models.py
@@ -7,8 +7,10 @@ import string
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    date_ordered = models.DateTimeField(auto_now_add=True)
+    date_ordered = models.DateTimeField(auto_now=True)
     complete = models.BooleanField(default=False)
+    delivered = models.BooleanField(default=False)
+    date_delivered = models.DateTimeField(null=True, blank=True, auto_now=True)
     transaction_id = models.CharField(max_length=250, null=True)
 
     def __str__(self):
