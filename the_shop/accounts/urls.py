962bcc13c28b364be23d9f7d590f36cf8142c7b4
zebbyG <zebbynjau@gmail.com>
2023-07-14 17:34:40 +0300

    Add password reset urls

--- the_shop/accounts/urls.py
+++ the_shop/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
@@ -11,5 +12,10 @@ urlpatterns = [
     path('profile_details/', views.profile_details, name="profile_details"),
     path('password_change/', views.password_change, name='password_change'),
     path('delete_profile_picture/', views.delete_profile_picture, name="delete_profile_picture"),
-    path('log_out/', views.log_out, name="log_out")
+    path('log_out/', views.log_out, name="log_out"),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
 ]
