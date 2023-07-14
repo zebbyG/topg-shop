ad38e200d661b3733f63f956c2aa8d7ad2292b85
zebbyG <zebbynjau@gmail.com>
2023-07-14 17:36:45 +0300

    Add urls for more products

--- the_shop/brands/urls.py
+++ the_shop/brands/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path('Vans/', views.vans, name="Vans"),
     path('Yeezys/', views.yeezys, name="Yeezys"),
     path('Pradas/', views.pradas, name="Pradas"),
-    path('shoes_accessories/', views.shoes_accessories, name="shoes_accessories"),
+    path('Shoes Accessories/', views.shoes_accessories, name="Shoes Accessories"),
+    path('Pumas/', views.pumas, name="Pumas"),
+    path('Balenciagas/', views.balenciagas, name="Balenciagas"),
+    path('Gucci/', views.gucci, name="Gucci"),
+    path('Louis Vuitton/', views.louis_vuitton, name="Louis Vuitton"),
+    path('Versace/', views.versace, name="Versace"),
+    path('Addidas/', views.addidas, name="Addidas"),
     path('', views.catalogue, name="catalogue"),
 ]
