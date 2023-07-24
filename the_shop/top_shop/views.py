c474e19cad555120f80bf7d99f662ddb604d1a17
zebbyG <zebbynjau@gmail.com>
2023-07-24 12:36:17 +0300

    views.py

--- the_shop/top_shop/views.py
+++ the_shop/top_shop/views.py
@@ -3,6 +3,20 @@ from theorders.models import Order
 
 
 def home(request):
+    # # Get the latest shoes for each brand
+    # latest_shoes = Product.objects.filter(
+    #     brand=OuterRef('brand')
+    # ).order_by('-shoe_time_added')
+    #
+    # # Get the distinct brands with their latest shoes
+    # distinct_brands_with_latest_shoes = Product.objects.filter(
+    #     shoe_time_added__lte=datetime.now(),  # To avoid future timestamps if any
+    # ).annotate(
+    #     latest_shoe_id=Subquery(latest_shoes.values('id')[:1])
+    # ).values('latest_shoe_id')
+    #
+    # # Fetch the final queryset with the latest shoes for each brand
+    # popular_products = Product.objects.select_related('brand').filter(id__in=distinct_brands_with_latest_shoes.values('latest_shoe_id'))
     if request.user.is_authenticated:
         customer = request.user
         order, created = Order.objects.get_or_create(user=customer, complete=False)
