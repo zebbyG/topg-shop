84acdd2fd3b0ea6bab1380cfaf6efc96a9e9d4dc
zebbyG <zebbynjau@gmail.com>
2023-07-27 15:15:09 +0300

    urls for password resetting

--- the_shop/accounts/urls.py
+++ the_shop/accounts/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('delete_profile_picture/', views.delete_profile_picture, name="delete_profile_picture"),
     path('log_out/', views.log_out, name="log_out"),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name="password_reset_confirm"),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name="password_reset_complete")
 ]
