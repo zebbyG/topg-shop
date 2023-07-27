037793c7b806f89294be6663ab4ecbc78396e500
zebbyG <zebbynjau@gmail.com>
2023-07-27 15:16:17 +0300

    Added django view urls

--- the_shop/the_shop/urls.py
+++ the_shop/the_shop/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('catalogue/', include('brands.urls')),
     path('account/', include('accounts.urls')),
     path('orders/', include('theorders.urls')),
+    path('account/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
