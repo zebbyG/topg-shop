0b062b66463d2a749070f28dae7b23c0408d8bc1
zebbyG <zebbynjau@gmail.com>
2023-07-27 15:15:52 +0300

    Added Smtp configurations

--- the_shop/the_shop/settings.py
+++ the_shop/the_shop/settings.py
@@ -150,8 +150,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zebbynjau@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('USER_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('USER_PASSWORD')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
