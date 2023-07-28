020fc8cf34ff93c2b8471f0329d2c95df2a1197b
zebbyG <zebbynjau@gmail.com>
2023-07-28 19:11:56 +0300

    try post requests

--- the_shop/the_shop/settings.py
+++ the_shop/the_shop/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'theorders',
 ]
 
+# CSRF_TRUSTED_ORIGINS = [
+#
+# ]
+
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
@@ -56,7 +60,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
