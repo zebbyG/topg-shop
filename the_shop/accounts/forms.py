bcabdfad6a667db60b33e1046e6627f4c94a6e1c
zebbyG <zebbynjau@gmail.com>
2023-07-29 14:53:02 +0300

    To only check email duplicates if "email" is changed

--- the_shop/accounts/forms.py
+++ the_shop/accounts/forms.py
@@ -35,8 +35,9 @@ class EditProfileForm(UserChangeForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email address is already in use.")
+        if self.is_valid() and 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("This email address is already in use.")
         return email
 
     class Meta:
