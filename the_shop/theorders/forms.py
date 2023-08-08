1a093969a9fd70bcd99511c7108b6d89e33a0879
zebbyG <zebbynjau@gmail.com>
2023-08-08 16:16:14 +0300

    max of 500 char in comment section

--- the_shop/theorders/forms.py
+++ the_shop/theorders/forms.py
@@ -4,7 +4,7 @@ from .models import Comment
 
 class CommentForm(forms.ModelForm):
     title = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
-    comment = forms.CharField(max_length=100, widget=forms.Textarea(attrs={'class': 'form-control'}))
+    comment = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Comment
