01779d3b1916dbad035066eac486ed8c58a95daa
zebbyG <zebbynjau@gmail.com>
2023-08-08 14:05:19 +0300

    leave a comment url

--- the_shop/theorders/urls.py
+++ the_shop/theorders/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('process_order/', views.process_order, name="process_order"),
     path('my_orders/', views.complete_orders, name="my_orders"),
     path('order_completed/', views.order_complete, name="order_completed"),
+    path('leave_a_comment/', views.leave_comment, name="leave_a_comment")
     # path('delete_order/', views.delete_order, name='delete_order')
 ]
