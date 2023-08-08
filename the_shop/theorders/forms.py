9ff7c5087502cdade43c6d68bd698c44459c6de5
zebbyG <zebbynjau@gmail.com>
2023-08-08 14:03:39 +0300

    leave a comment form

---
+++ the_shop/theorders/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    title = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    comment = forms.CharField(max_length=100, widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Comment
+        fields = ('title', 'comment')
