281d7c3dd2750c23e55dfa6fb970d1df77c40bea
zebbyG <zebbynjau@gmail.com>
2023-08-10 09:41:37 +0300

    Add model Messages

--- the_shop/top_shop/models.py
+++ the_shop/top_shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField()
+    message = models.CharField(max_length=2500)
+
+    def __str__(self):
+        return f'A message from {self.email}'
